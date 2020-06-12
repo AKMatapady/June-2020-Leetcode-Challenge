05f12e5c5bd2957385af72fe74f1def309f1a3bf
AKM_Curious <amithkmbvr@gmail.com>
2020-06-12 16:30:12 -0500

    updated previous commit

--- Week 2/Insert Delete GetRandom O(1).java
+++ Week 2/Insert Delete GetRandom O(1).java
@@ -15,24 +15,20 @@ class RandomizedSet {
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        boolean ret = st.contains(val);
-        if(!ret)
-        {
-            st.add(val);
-            l.add(val);
-        }        
-        return !ret;
+        if(st.contains(val))
+            return false;            
+        st.add(val);
+        l.add(val);
+        return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        boolean ret = st.contains(val);
-        if(ret)
-        {
-            st.remove(val);
-            l.remove(((Integer)val));
-        }        
-        return ret;
+        if(!st.contains(val))
+            return false;        
+        st.remove(val);
+        l.remove(((Integer)val));
+        return true;
     }
     
     /** Get a random element from the set. */
